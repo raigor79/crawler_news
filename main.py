07ac08ff152951bae7e9a645f1761eaf7a8c316b
raigor <raigor@mail.ru>
2021-08-19 19:43:20 +0300

    add header region and language in request

--- main.py
+++ main.py
@@ -20,7 +20,12 @@ ERRORS = {
 }
 
 
-URL_NEWS = 'https://news.google.com/search?q=Russia&hl=en-US&gl=US&ceid=US%3Aen'
+parametrs_parser = {
+    'key_word': 'Russia',
+    'language': 'en',
+    'period': '1y',
+    'region': 'US'    
+}
 
 
 logger.add(
@@ -40,31 +45,41 @@ async def fetch_page(client, url):
         async with client.get(url) as resp:
             if resp.status in (OK,):
                 logger.info(
-                    f'Home page loaded, status response = {resp.status} : {ERRORS[resp.status]}'
+                    f'Page {url[:20]}... loaded, status response = {resp.status} : {ERRORS[resp.status]}'
                     )
                 home_page_text = await resp.text()
             else:
                 logger.info(
-                    f'Home page is not loaded, status error {resp.status} : {ERRORS[resp.status]}'
+                    f'Page {url[:20]}... is not loaded, status error {resp.status} : {ERRORS[resp.status]}'
                     )
     except Exception:
         pass
+    return home_page_text
 
 
-async def main():
+async def main(par):
     timeout = aiohttp.ClientTimeout(total=30)
     conn = aiohttp.TCPConnector(limit_per_host=30)
+    user_agent = 'Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:64.0) Gecko/20100101 Firefox/64.0'
+    accept_language= par['language'] + '-' + par['region'] + ',' + par['language'] + ';q=0.9'
+    header_request = {'User-Agent': user_agent, 'Accept-Language': accept_language}
     async with aiohttp.ClientSession(
         timeout=timeout, 
-        connector=conn
+        connector=conn,
+        headers=header_request
         ) as client:
-        text_page = await fetch_page(client, URL_NEWS)
+        url_news = 'https://news.google.com/search?q={}+when:{}&hl={}'.format(
+            par['key_word'],
+            par['period'],
+            par['language']
+        )
+        text_page = await fetch_page(client, url_news)
 
 
 if __name__ == "__main__":
     logger.info('Start - >')
     try:    
-        asyncio.run(main())
+        asyncio.run(main(parametrs_parser))
     except KeyboardInterrupt:
         pass
     logger.info('< - Close')
