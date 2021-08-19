f004f1c240fd00b1dace377d6576c9cd18c9de45
raigor <raigor@mail.ru>
2021-08-19 03:09:06 +0300

    add function main, which load page

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 import sys
 import asyncio
 import aiohttp
 from loguru import logger
+
+
+URL_NEWS = 'https://news.google.com/search?q=Russia&hl=en-US&gl=US&ceid=US%3Aen'
 
 
 logger.add(
@@ -12,13 +16,19 @@ logger.add(
         )
 
 
-def main():
-    pass
+async def main():
+    timeout = aiohttp.ClientTimeout(total=30)
+    conn = aiohttp.TCPConnector(limit_per_host=30)
+    async with aiohttp.ClientSession(timeout=timeout, connector=conn) as client:
+        async with client.get(URL_NEWS) as resp:
+            print(resp.status)
+            print(await resp.text())
+
 
 if __name__ == "__main__":
     logger.info('Start - >')
     try:    
-        pass
+        asyncio.run(main())
     except KeyboardInterrupt:
         pass
     logger.info('< - Close')
