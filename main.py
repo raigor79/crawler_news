555daccb24868b8ed463cf9fd0a2e62e38e0a5a8
raigor <raigor@mail.ru>
2021-08-19 03:52:41 +0300

    add functions fetch_page

--- main.py
+++ main.py
@@ -5,6 +5,22 @@ import aiohttp
 from loguru import logger
 
 
+OK = 200
+BAD_REQUEST = 400
+FORBIDDEN = 403
+NOT_FOUND = 404
+INVALID_REQUEST = 422
+INTERNAL_ERROR = 500
+ERRORS = {
+    OK: "Error have been not",
+    BAD_REQUEST: "Bad Request",
+    FORBIDDEN: "Forbidden",
+    NOT_FOUND: "Not Found",
+    INVALID_REQUEST: "Invalid Request",
+    INTERNAL_ERROR: "Internal Server Error",
+}
+
+
 URL_NEWS = 'https://news.google.com/search?q=Russia&hl=en-US&gl=US&ceid=US%3Aen'
 
 
@@ -16,13 +32,34 @@ logger.add(
         )
 
 
+def parsing_page(text: str) -> list[str]:
+    ...
+
+
+async def fetch_page(client, url):
+    try:
+        async with client.get(url) as resp:
+            if resp.status in (OK,):
+                logger.info(
+                    f'Home page loaded, status response = {resp.status} : {ERRORS[resp.status]}'
+                    )
+                home_page_text = await resp.text()
+            else:
+                logger.info(
+                    f'Home page is not loaded, status error {resp.status} : {ERRORS[resp.status]}'
+                    )
+    except Exception:
+        pass
+
+
 async def main():
     timeout = aiohttp.ClientTimeout(total=30)
     conn = aiohttp.TCPConnector(limit_per_host=30)
-    async with aiohttp.ClientSession(timeout=timeout, connector=conn) as client:
-        async with client.get(URL_NEWS) as resp:
-            print(resp.status)
-            print(await resp.text())
+    async with aiohttp.ClientSession(
+        timeout=timeout, 
+        connector=conn
+        ) as client:
+        text_page = await fetch_page(client, URL_NEWS)
 
 
 if __name__ == "__main__":
